2f93c94440e336d2169867383b032588feea60f2
SimonX13 <66461376+SimonX13@users.noreply.github.com>
2022-05-12 15:30:49 -0700

    Delete src directory

--- src/Image.js
+++
@@ -1,7 +0,0 @@
-// function Image(props) {
-//     return <h1>Hello, {props.source}</h1>;
-//   }
-  
-//   const root = ReactDOM.createRoot(document.getElementById('root'));
-//   const element = <Welcome source="/Cascara_Cherry.jpg" />;
-//   root.render(element);
