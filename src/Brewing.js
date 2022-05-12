955a94e7ed63148b0bb46f1a798858ccf4c318e7
SimonX13 <66461376+SimonX13@users.noreply.github.com>
2022-05-12 15:26:31 -0700

    Add files via upload

---
+++ src/Brewing.js
@@ -0,0 +1,10 @@
+const Brewing = () => {
+    return (
+        <div>
+            <img src="/Cascara_Cherry.jpg" alt="coffee bean image"/>
+            <p>This is the earliest stage of a coffee bean</p>
+        </div>
+    );
+};
+
+export default Brewing;
---
+++ src/Product.js
@@ -0,0 +1,10 @@
+const Product = () =>{
+    return (
+        <div>
+            <img src="/product.jpg" alt="sdfg"/>
+            <h6> a cup of coffee</h6>
+        </div>
+        
+    );
+};
+export default Product;
